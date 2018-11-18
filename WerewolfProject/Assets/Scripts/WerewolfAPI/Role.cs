11b5c893b37a3aa832c3af09185068b7e4ed9a31
NirushaaaR <nirush_srijaimoon@cmu.ac.th>
2018-11-18 20:48:18 +0700

    add all api model

--- WerewolfProject/Assets/Scripts/WerewolfAPI/Role.cs
+++ WerewolfProject/Assets/Scripts/WerewolfAPI/Role.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections.Generic;
 
-
 namespace WerewolfAPIModel
 {
     [System.Serializable]
     public class Role
     {
-
+        public long? id;
+        public string name;
+        public string description;
+        public List<Action> actions;
+        public string type;
 
     }
 }
